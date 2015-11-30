9ec0cb84f69cc3d12379142542a6d2219ba47798
Nelson Daniel Durán Morel <Nelson Daniel Durn Morel>
2015-11-30 19:06:54 -0400

    listCountries urls.py puse countries

--- listCountries/urls.py
+++ listCountries/urls.py
@@ -17,6 +17,6 @@ from django.conf.urls import include, url
 from django.contrib import admin
 
 urlpatterns = [
-	url(r'^listCountries/', include('countries.urls',namespace="countries")),
+    url(r'^countries/', include('countries.urls', namespace="countries")),
     url(r'^admin/', include(admin.site.urls)),
 ]

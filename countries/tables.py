951299940e8f63972433f52267ca9947786b49bd
Nelson Daniel Durán Morel <Nelson Daniel Durn Morel>
2015-12-01 10:11:32 -0400

    Added tables.py changed contry.html and countries.views

---
+++ countries/tables.py
@@ -0,0 +1,8 @@
+﻿import django_tables2 as tables
+from .models import Country
+
+class CountryTable(tables.Table):
+    class Meta:
+        model = Country
+        # add class="paleblue" to <table> tag
+        attrs = {"class": "paleblue"}

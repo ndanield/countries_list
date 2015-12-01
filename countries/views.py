d00385a7b16cd992fefd91bbf5e3c09660a8fafe
Nelson Daniel Durán Morel <Nelson Daniel Durn Morel>
2015-12-01 12:30:22 -0400

    fixed view.py

--- countries/views.py
+++ countries/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 from .models import Country
 
 def country(request):
-	return render(request, 'country.html', {'contry': Country.objects.all()})
+	return render(request, "country.html", {"country": Country.objects.all()})

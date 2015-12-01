0c01a2ecd073a4ac49c1368f2eea9aec9de1eb4e
Nelson Daniel Durán Morel <Nelson Daniel Durn Morel>
2015-12-01 12:24:37 -0400

    cambia algunas cosas

--- countries/tables.py
+++
@@ -1,8 +0,0 @@
-﻿import django_tables2 as tables
-from .models import Country
-
-class CountryTable(tables.Table):
-    class Meta:
-        model = Country
-        # add class="paleblue" to <table> tag
-        attrs = {"class": "paleblue"}
--- countries/views.py
+++ countries/views.py
@@ -1,10 +1,5 @@
 from django.shortcuts import render
 from .models import Country
-from django_tables2 import RequestConfig
-from .tables import CountryTable
 
 def country(request):
-	#"country": Country.objects.all()
-	table = CountryTable(Country.objects.all())
-	RequestConfig(request).configure(table)
-	return render(request, 'country.html', {'table': table})
+	return render(request, 'country.html', {'contry': Country.objects.all()})

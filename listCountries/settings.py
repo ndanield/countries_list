aa8515e795976ae92137b77c0fd9fab4c196aa32
Nelson Daniel Durán Morel <Nelson Daniel Durn Morel>
2015-12-01 23:23:14 -0400

    Django_urls final de settings.py

--- listCountries/settings.py
+++ listCountries/settings.py
@@ -78,12 +78,15 @@ WSGI_APPLICATION = 'listCountries.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.8/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+
+import dj_database_url
+DATABASES['default'] =  dj_database_url.config()
+# DATABASES = {
+    # 'default': {
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    # }
+# }
 
 
 # Internationalization
@@ -105,5 +108,4 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-import dj_database_url
-DATABASES['default'] =  dj_database_url.config()
+
